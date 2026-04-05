0b4bdca480e6d76e828486a30b598add3e5f9e05
shrawi10317 <shrawaniw03@gmail.com>
2026-04-05 14:41:08 +0530

    fix deps

--- app/__init__.py
+++ app/__init__.py
@@ -29,9 +29,3 @@ def create_app():
     return app
 
 
-@app.after_request
-def add_header(response):
-    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
-    response.headers["Pragma"] = "no-cache"
-    response.headers["Expires"] = "0"
-    return response
